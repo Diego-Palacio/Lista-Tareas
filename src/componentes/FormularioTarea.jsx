8281b5634ba72d49f755cba62bc9944913a262d3
diego-palacio <diego_palacio2015@hotmail.com>
2023-11-19 23:34:19 -0300

    agrego estilo y pruebo funcionalidad de agregar tareas

--- src/componentes/FormularioTarea.jsx
+++ src/componentes/FormularioTarea.jsx
@@ -1,6 +1,16 @@
 
 export const FormularioTarea = () => {
+
+    const agregarTarea = (e) =>{
+        console.log("Tarea agregada")
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
   return (
-    <div>formularioTarea</div>
+        <form onSubmit={agregarTarea}>
+            <input type="text" name="texto" style={{width:250+"px", marginRight:10+"px",marginBottom:10+"px"}}/>
+            <button type="submit"> Agregar tarea </button>
+        </form>
   )
 }
--- src/componentes/Tarea.jsx
+++ src/componentes/Tarea.jsx
@@ -1,6 +1,26 @@
+import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import { Fragment } from "react";
 
 export const Tarea = () => {
-  return (
-    <div>tarea</div>
-  )
+   
+
+    return (
+            <>
+                <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea> 
+                    <CardContent>
+
+                    <Typography gutterBottom variant="h5" component="div">
+                        Titulo de tarea
+                    </Typography>
+
+                    <Typography variant="body2" color="text.secondary">
+                        Desarrollo de la tarea
+                    </Typography>
+
+                    </CardContent>
+                </CardActionArea>
+                </Card>
+            </>            
+        );
 }
